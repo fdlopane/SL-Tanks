4166a0c9963799ac7c615eeb95d381272ff8fdd6
fulviolopane <fulvio.lopane@gmail.com>
2023-10-19 10:44:06 +0100

    removed useless code

--- geocomputation.py
+++ geocomputation.py
@@ -45,69 +45,6 @@ def resample_raster(input_path, output_path, x_resolution, y_resolution):
         with rasterio.open(output_path, 'w', **kwargs) as dst:
             dst.write(data)
 
-########################################################################################################################
-
-'''
-from osgeo import gdal
-
-def raster_to_shp(input_raster, raster_band, field_name, output_shp):
-    # Open the raster dataset
-    raster_ds = gdal.Open(input_raster)
-    band = raster_ds.GetRasterBand(raster_band)
-
-    # Get raster geotransformation information
-    transform = raster_ds.GetGeoTransform()
-
-    # Create an empty GeoDataFrame
-    points_gdf = gpd.GeoDataFrame(columns=[field_name], geometry=gpd.GeoSeries([], name='geometry'))
-
-    # Loop through each pixel in the raster
-    for y in range(raster_ds.RasterYSize):
-        for x in range(raster_ds.RasterXSize):
-            value = band.ReadAsArray(x, y, 1, 1)[0, 0]
-
-            if value is not None:
-                # Convert raster coordinates to geographic coordinates
-                x_geo = transform[0] + x * transform[1] + y * transform[2]
-                y_geo = transform[3] + x * transform[4] + y * transform[5]
-
-                # Append a new row to the GeoDataFrame
-                points_gdf = points_gdf._append({field_name: int(value), 'geometry': gpd.points_from_xy([x_geo], [y_geo])[0]}, ignore_index=True)
-
-    # Set the GeoDataFrame's coordinate reference system
-    points_gdf.crs = raster_ds.GetProjection()
-
-    # Save the GeoDataFrame to a shapefile
-    points_gdf.to_file(output_shp)
-'''
-
-### Joe's method:
-def raster_to_shp_THIS_WORKS(input_raster, output_shp):
-    with rasterio.open(input_raster) as src:
-        raster_data = src.read(1)  # Read the raster data and mask out any NoData values
-        transform = src.transform  # Get the transformation matrix to convert pixel coordinates to geographic coordinates
-        raster_crs = src.crs
-
-    # Get the number of rows and columns in the raster:
-    num_rows, num_cols = raster_data.shape
-
-    # Create a regular grid of points based on the raster's extent and resolution:
-    x_coords, y_coords = np.meshgrid(np.arange(0, num_cols), np.arange(0, num_rows))
-    points = np.c_[x_coords.ravel(), y_coords.ravel()]
-
-    # Transform the grid of points from pixel coordinates to geographic coordinates
-    lon, lat = rasterio.transform.xy(transform, points[:, 1], points[:, 0])
-
-    # Get the raster values at each point location
-    raster_values = raster_data.ravel()
-
-    # Create a GeoDataFrame with the points and set the CRS to match the raster
-    geometry = [Point(lon[i], lat[i]) for i in range(len(lon))]
-    gdf_pop = gpd.GeoDataFrame({'geometry': geometry, 'raster_value': raster_values}, crs=raster_crs)
-    print('Worldpop raster converted into points')
-
-    gdf_pop.to_feather(output_shp)
-
 def raster_to_shp(input_raster, output_shp, field_name:str):
     with rasterio.open(input_raster) as src:
         raster_data = src.read(1)  # Read the raster data and mask out any NoData values
